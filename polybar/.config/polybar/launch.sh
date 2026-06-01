98c069e0fda4a7baa94301c25f6d51585f3d3490
Jim Moult <moult.jim@gmail.com>
2026-05-31 23:36:42 -0400

    add md cheat

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -3,7 +3,10 @@
 killall -q polybar
 # wait until processes shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-# launch polybar, using defaults in
+# giv xorg/i3 time to map screens (on cold boot)
+sleep 0.5
+# launch polybar, using defaults inc
 # ~/.config/polybar/config.ini
-polybar example &
+polybar example 2>&1 | tee -a /tmp/polybar.log & disown
+#polybar example &
 
