41cef11ba003bbed056001e25edee069ab188336
Jim Moult <moult.jim@gmail.com>
2026-06-13 09:52:29 -0400

    maint

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -4,9 +4,9 @@ killall -q polybar
 # wait until processes shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # giv xorg/i3 time to map screens (on cold boot)
-sleep 0.5
-# launch polybar, using defaults inc
-# ~/.config/polybar/config.ini
+sleep 1
+# Clear the old log file (or create it if it doesn't exist)
+> /tmp/polybar.log
+# launch
 polybar example 2>&1 | tee -a /tmp/polybar.log & disown
-#polybar example &
 
